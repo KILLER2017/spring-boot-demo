f2abf5f798f8b5430e0a5493031ebc23d4e2e2dc
ALVIN <543046534@qq.com>
2020-10-27 11:04:19 +0800

    修复科研绩效规则-科研获奖：勾选为“采用”后点击保存，并没有更新内容

--- src/main/java/top/alvinsite/demo/controller/rule/AwardedRuleController.java
+++ src/main/java/top/alvinsite/demo/controller/rule/AwardedRuleController.java
@@ -4,7 +4,6 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +15,6 @@ import top.alvinsite.demo.service.rule.AwardedRuleService;
 import xcz.annotation.PermissionClass;
 
 import javax.validation.Valid;
-import java.util.List;
 
 
 /**
@@ -40,10 +38,5 @@ public class AwardedRuleController extends BaseRuleController<AwardedRuleService
     public RuleVO get(@RequestHeader("authorization") UserInfo userInfo,
                       @ApiParam(value = "desc of param" , required=true ) @Valid RuleQuery ruleQuery) {
         return super.get(userInfo, ruleQuery);
-    }
-
-    @Override
-    public void post(@RequestHeader("authorization") UserInfo userInfo, @Valid RuleQuery ruleQuery, boolean useScoreDistribution, @RequestBody @Valid List<AwardedRule> rules) {
-        super.post(userInfo, ruleQuery, useScoreDistribution, rules);
     }
 }
