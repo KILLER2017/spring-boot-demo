d5bceb23202eadc66e581ae824a92ea973b8fba7
ALVIN <543046534@qq.com>
2020-11-18 15:42:25 +0800

    【优化】薪酬人员列表，修改为按更新时间降序，工号升序排序

--- src/main/java/top/alvinsite/framework/amqp/Runner.java
+++ src/main/java/top/alvinsite/framework/amqp/Runner.java
@@ -4,9 +4,6 @@ package top.alvinsite.framework.amqp;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-import top.alvinsite.framework.amqp.config.RabbitmqConfig;
-
-import java.util.concurrent.TimeUnit;
 
 
 /**
@@ -25,9 +22,9 @@ public class Runner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("Sending message...");
-        rabbitTemplate.convertAndSend(RabbitmqConfig.topicExchangeName, "foo.bar.baz", "Hello from RabbitMQ!");
-        receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
+        // System.out.println("Sending message...");
+        // rabbitTemplate.convertAndSend(RabbitmqConfig.topicExchangeName, "foo.bar.baz", "Hello from RabbitMQ!");
+        // receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
     }
 
 }
