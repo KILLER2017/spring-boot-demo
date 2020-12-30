c6e60232ac75cfc257e5f3a506fb0e0ecd523df7
ALVIN <543046534@qq.com>
2020-12-30 14:46:40 +0800

    【调整】修复已知BUG

---
+++ README.md
@@ -0,0 +1,5 @@
+# 项目说明
++ 原型：http://113.105.128.165:1314/fixed/61/f7f04f8cbd57475c36868e8a9a1dfc5b/index.html
++ API文档： https://documenter.getpostman.com/view/3928620/TVsviStg
++ 测试服：http://172.31.200.1:10300
++ 正式服：http://csjx.dgut.edu.cn
--- src/main/java/top/alvinsite/demo/model/params/salary/OvertimeWorkedSubsidyUpdateParam.java
+++ src/main/java/top/alvinsite/demo/model/params/salary/OvertimeWorkedSubsidyUpdateParam.java
@@ -1,6 +1,7 @@
 package top.alvinsite.demo.model.params.salary;
 
 import lombok.Data;
+import top.alvinsite.demo.model.support.ExcelColumn;
 
 /**
  * @author Alvin
@@ -10,7 +11,15 @@ public class OvertimeWorkedSubsidyUpdateParam {
 
     private Integer id;
 
+    @ExcelColumn(value = "姓名", col = 1, width = 10)
+    private String nickname;
+
+    @ExcelColumn(value = "工号", col = 2, width = 10)
+    private String account;
+
+    @ExcelColumn(value = "专业技术职务", col = 3)
     private String professionalPosition;
 
+    @ExcelColumn(value = "实际完成课程教学工作量", col = 4)
     private Integer teachingWorkload;
 }
