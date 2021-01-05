090263ab8025575a4850d32829ee7ca36779f58f
ALVIN <543046534@qq.com>
2021-01-05 10:51:47 +0800

    【修复】工资详情列表接口报错的问题

--- src/main/java/top/alvinsite/demo/controller/salary/JobSubsidyController.java
+++ src/main/java/top/alvinsite/demo/controller/salary/JobSubsidyController.java
@@ -1,13 +1,17 @@
 package top.alvinsite.demo.controller.salary;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import top.alvinsite.demo.model.entity.salary.JobSubsidy;
 import top.alvinsite.demo.model.params.PerformanceQuery;
 import top.alvinsite.demo.model.params.salary.JobSubsidyUpdateParam;
 import top.alvinsite.demo.service.salary.JobSubsidyService;
+
+import static top.alvinsite.utils.BeanUtils.transformFrom;
 
 /**
  * 岗位津贴控制器
@@ -54,5 +58,26 @@ public class JobSubsidyController extends BaseSalaryController<JobSubsidyService
         // 计算并设置个人岗位津贴
         entity.setSubsidy(entity.getSubsidyFactor() * entity.getLength());
         return entity;
+    }
+
+    @Override
+    public void update(@RequestBody JobSubsidyUpdateParam record) {
+        JobSubsidy item = transformFrom(record, getEntityClass());
+        assert item != null;
+        String[] parameters = item.getId().split("#");
+        item.setId(null);
+        item.setYear(Integer.valueOf(parameters[0]));
+        item.setAccount(parameters[1]);
+        item.setSubsidy(item.getSubsidyFactor() * item.getLength());
+
+        QueryWrapper<JobSubsidy> queryWrapper = new QueryWrapper<JobSubsidy>()
+                .eq("account", item.getAccount())
+                .eq("year", item.getYear());
+        baseService.saveOrUpdate(item, queryWrapper);
+    }
+
+    @Override
+    public void updateByPost(@RequestBody JobSubsidyUpdateParam record) {
+        update(record);
     }
 }
